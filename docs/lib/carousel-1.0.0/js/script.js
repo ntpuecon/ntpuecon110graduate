96675a5ebbf87a9eece6ae5476d18e88f6cb9bb8
tpemartin <tpemartin@gmail.com>
2022-06-13 13:03:48 +0800

    update

--- docs/lib/carousel-1.0.0/js/script.js
+++ docs/lib/carousel-1.0.0/js/script.js
@@ -1,4 +1,7 @@
-const divInstall = document.getElementById('installContainer');
+
+
+$(function(){
+  const divInstall = document.getElementById('installContainer');
 const butInstall = document.getElementById('butInstall');
 
 /* Put code here */
@@ -31,7 +34,29 @@ if (window.location.protocol === 'http:') {
   requireHTTPS.classList.remove('hidden');
 }
 
-$(function(){
+butInstall.addEventListener('click', async () => {
+  console.log('👍', 'butInstall-clicked');
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    // The deferred prompt isn't available.
+    return;
+  }
+  // Show the install prompt.
+  promptEvent.prompt();
+  // Log the result
+  const result = await promptEvent.userChoice;
+  console.log('👍', 'userChoice', result);
+  // Reset the deferred prompt variable, since
+  // prompt() can only be called once.
+  window.deferredPrompt = null;
+  // Hide the install button.
+  divInstall.classList.toggle('hidden', true);
+});
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event);
+  // Clear the deferredPrompt so it can be garbage collected
+  window.deferredPrompt = null;
+});
 	// ---- this would be the plugin ----
 	// special event for non-preventDefault
 	jQuery.event.special.touchstart = {
