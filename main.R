34909b12f6e48fba4a23df096ffa4cc6c742567f
tpemartin <tpemartin@gmail.com>
2022-06-16 10:45:40 +0800

    update

--- main.R
+++ main.R
@@ -15,4 +15,23 @@ pwa_index() |> app$test()
 
 # app move
 app$move("temp", "docs") -> result
-debug(app$move)
+
+# ----- Post Change ----
+devtools::load_all(".")
+apptest:::startAppTest()
+apptest::pwa()
+# change PWA manifest
+pwa$json |> View()
+pwa$json$name="NTPU Economics Wish 2022"
+pwa$json$short_name="NESH 2022"
+pwa$json$description="這是給國立臺北大學經濟學系2022年學士班及進修學士班畢業生互道畢業祝福的App ( N.tpu E.conomics wi.SH，簡稱NESH)。"
+pwa$json$scope="/ntpuecon110graduate"
+pwa$updateManifest()
+debug(pwa$updateManifest)
+pwa_index() |> app$test()
+page_form() |> app$test("form.html")
+
+
+apptest:::startAppTest()
+app$move("temp", "docs") -> result
+servr::httd("docs")
