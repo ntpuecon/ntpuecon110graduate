6427f2c06b4fa33b41447adb0148ea04decedb57
tpemartin <tpemartin@gmail.com>
2022-06-16 21:38:41 +0800

    update

--- main.R
+++ main.R
@@ -7,13 +7,17 @@ page_form() |> app$test("form.html")
 
 # pwa create
 apptest::pwa()
+## use page_index() tag to create a PWA in temp folder
 pwa$createPWA(
   page_index(), "temp"
 )
+## feed in 512x512px icon for icons generation, and update webmanifest accordingly
 pwa$generateIconsFrom512pxIcon("/Users/martinl/Github/ntpuecon110graduate/inst/pwa/icons/ntpuecon110graduate512.png")
+## check the pwa version of page_index
 pwa_index() |> app$test()
 
-# app move
+
+## When satisfied with the test, move app to the publishing folder:
 app$move("temp", "docs") -> result
 
 # ----- Post Change ----
@@ -25,13 +29,20 @@ pwa$json |> View()
 pwa$json$name="NTPU Economics Wish 2022"
 pwa$json$short_name="NESH 2022"
 pwa$json$description="這是給國立臺北大學經濟學系2022年學士班及進修學士班畢業生互道畢業祝福的App ( N.tpu E.conomics wi.SH，簡稱NESH)。"
-pwa$json$scope="/ntpuecon110graduate"
+pwa$json$scope="/"
+pwa$json$start_url="index.html"
+pwa$json$orientation="portrait"
 pwa$updateManifest()
-debug(pwa$updateManifest)
 pwa_index() |> app$test()
 page_form() |> app$test("form.html")
 
 
 apptest:::startAppTest()
 app$move("temp", "docs") -> result
-servr::httd("docs")
+
+
+
+
+# change UI  --------------------------------------------------------------
+
+
