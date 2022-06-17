a06ab7b5667d3ae2177e8549f8c3096056a5fcb4
tpemartin <tpemartin@gmail.com>
2022-06-18 07:51:02 +0800

    update

--- R/sidenavTemp.R
+++ R/sidenavTemp.R
@@ -67,15 +67,16 @@ flex-grow: 0;
     ),
     tags$div(class="designer",
     tags$div(class="story",
-      tags$p("Ntpu Economic wiSH（簡稱NESH）是給國立臺北大學學士班及進修學士班互留祝福的App，每一則祝福種出一顆綠樹。點擊樹可叫出種樹的祝福卡，雙擊卡片可叫出所種下的樹。畢業後同學也可透過此App，更新自己的訊息，讓彼此知道自己的近況。"))
+      tags$p("Ntpu Economic wiSH（簡稱NESH）是給國立臺北大學學士班及進修學士班互留祝福的App，每一則祝福種出一顆綠樹。點擊樹可叫出種樹的祝福卡，雙擊卡片可叫出所種下的樹。畢業後同學也可透過此App，更新自己的訊息，讓彼此知道自己的近況。")),
 
-      ),
+
+    tags$div(class="divider"),
   tags$ul(
-    style="position: absolute;
-    bottom: 50px;
-}",
+#     style="position: absolute;
+#     bottom: 50px;
+# }",
     tags$li(
-      tags$a(class="btn","經濟系系友會LINE", href="https://page.line.me/?accountId=826wroqn", target="_blank")))
+      tags$a(class="btn","經濟系系友會LINE", href="https://page.line.me/?accountId=826wroqn", target="_blank"))))
 
   ) #end of tagList
 }
